eb6e16007d4d1a2dedb973ac9c4595db287ef3b9
AntonyCoder <lysenko.tosha@gmail.com>
2021-02-24 13:43:19 +0300

    feat: add solution for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const currentConfig = bracketsConfig;
+    const toOpen = currentConfig.slice().map((item) => item[0]);
+    const toClose = currentConfig.slice().map((item) => item[1]);
+    let strToCheck = str;
+    let arrayFromStr = strToCheck.split("");
+    let opened = [];
+
+    for (let i = 0; i < arrayFromStr.length; i++) {
+        if (toOpen.includes(arrayFromStr[i])) {
+            const index = toOpen.indexOf(arrayFromStr[i]);
+            if (currentConfig[index][0] === currentConfig[index][1]) {
+                if (opened[opened.length - 1] === arrayFromStr[i]) {
+                    opened.pop();
+                } else {
+                    opened.push(arrayFromStr[i]);
+                }
+            } else {
+                opened.push(arrayFromStr[i]);
+            }
+        } else if (toClose.includes(arrayFromStr[i])) {
+            const closeIndex = toClose.indexOf(arrayFromStr[i]);
+            const openIndex = toOpen.indexOf(opened[opened.length - 1]);
+            if (closeIndex !== openIndex) {
+                return false;
+            }
+            opened.pop();
+        } else {
+            return "Error";
+        }
+    }
+
+    if (opened.length) {
+        return false;
+    }
+    return true;
+};
